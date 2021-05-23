e723a7419b9db4a2af455aad7ed061d170bd044f
hryeong66 <jhr110326@gmail.com>
2021-05-23 22:46:13 +0900

    [ADD] 수영 테스트앱 기본 프로젝트 추가

---
+++ swimming-test-app/swimming-test-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swimming-test-app
+//
+//  Created by 장혜령 on 2021/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
