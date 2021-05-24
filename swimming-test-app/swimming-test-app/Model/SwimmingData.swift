dc7272038f50ed169a31ca3018f2586307bef5dc
hryeong66 <jhr110326@gmail.com>
2021-05-24 17:15:08 +0900

    [ADD] swimming 데이터 처리 구조 추가

---
+++ swimming-test-app/swimming-test-app/Model/SwimmingData.swift
@@ -0,0 +1,15 @@
+//
+//  SwimmingData.swift
+//  swimming-test-app
+//
+//  Created by 장혜령 on 2021/05/24.
+//
+
+import Foundation
+
+struct SwimmingData{
+    var startDate: String
+    var endData: String
+    var distance: Double
+}
+
