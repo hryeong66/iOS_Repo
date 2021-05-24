edcedf0bc384e417ecf10a93f85f50b5e8362061
hryeong66 <jhr110326@gmail.com>
2021-05-24 17:14:25 +0900

    [ADD] 걷기+달리기 처리할 데이터 구조 추가

---
+++ swimming-test-app/swimming-test-app/Model/RunningData.swift
@@ -0,0 +1,14 @@
+//
+//  RunningData.swift
+//  swimming-test-app
+//
+//  Created by 장혜령 on 2021/05/24.
+//
+
+import Foundation
+
+struct RunningData{
+    var startDate: String
+    var endData: String
+    var distance: Double
+}
