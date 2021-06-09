5fc52aa83c41a2329cfff0eed4d75fb3ecf2a9d8
hryeong66 <jhr110326@gmail.com>
2021-06-10 00:27:59 +0900

    [CHORE] file Manager로 경로 설정 후 새로운 파일 생성하기

--- swimming-test-app-2/swimming-test-app-2/ViewController.swift
+++ swimming-test-app-2/swimming-test-app-2/ViewController.swift
@@ -15,13 +15,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func setNewCSVFile(){
+    func setNewCSVFile() -> URL{
         //fileManager 이용
+        //core data 도 함께
+
+        print(NSHomeDirectory())
+        let fileManager = FileManager.default // 파일 매니저의 싱글톤 객체 리턴
+        
+        // document dir url
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        print(documentsURL)
+        
+        let fileURL = documentsURL.appendingPathComponent("healthData.csv")
+        let myText = NSString(string:"")
+
+        //빈파일로 다시 만들기
+        try? myText.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8.rawValue)
+
+        
+        return fileURL
+        
     }
+    
+    
+    
 
     @IBAction func touchUpToStartWriter(_ sender: Any) {
+     
         
-        let stream = OutputStream(toFileAtPath: "file.csv", append: true)!
+        
+        let stream = OutputStream(url: documentURL , append: true)!
         let csv = try! CSVWriter(stream: stream)
 
         try! csv.write(row: ["id", "name"])
